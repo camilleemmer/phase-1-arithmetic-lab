4604fb4ea4f9cb590ac66d9a3d9b1acf23b58337
Camille Emmer <camilleemmer@gmail.com>
2022-06-18 15:14:26 -0600

    commit tree ea8c4066cc3aac34adb0316993c2f6f468202853

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+let num3 = 22;
+let num4 = 6;
+const mod = num3 % num4;
+
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
